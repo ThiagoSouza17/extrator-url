2e6d32349be693047943cacea782c2b3556c035f
Thiago Souza <thiagossouza07@gmail.com>
2022-02-13 19:34:49 -0300

    Realizando conversão de moeda usando valores da URL

--- extrator.py
+++ extrator.py
@@ -1,5 +1,6 @@
 import re
 
+
 class ExtratorURL:
     def __init__(self, url):
         self.url = self.sanitiza_url(url)
@@ -29,6 +30,25 @@ class ExtratorURL:
         url_parametros = self.url[indice_interrogacao + 1:]
         return url_parametros
 
+    def get_nome_parametro(self):
+        lista_de_nome_de_parametros = []
+        indice_e_comercial = self.get_url_parametros().find('&')
+        if indice_e_comercial == -1:
+            indice_parametro = self.get_url_parametros().find('=')
+            nome = self.get_url_parametros()[:indice_parametro]
+            lista_de_nome_de_parametros.append(nome)
+        else:
+            indice_parametro = self.get_url_parametros().find('=')
+            nome = self.get_url_parametros()[:indice_parametro]
+            lista_de_nome_de_parametros.append(nome)
+            indice_nome_anterior = 0
+            for num in range(self.get_url_parametros().count('=') - 1):
+                indice_e_comercial = self.get_url_parametros().find('&', indice_nome_anterior)
+                nome = self.get_url_parametros()[indice_e_comercial + 1:self.get_url_parametros().find('=', indice_e_comercial)]
+                indice_nome_anterior = indice_e_comercial + 1 + len(nome)
+                lista_de_nome_de_parametros.append(nome)
+        return lista_de_nome_de_parametros
+
     def get_valor_parametro(self, parametro_busca):
         indice_parametro = self.get_url_parametros().find(parametro_busca)
         indice_valor = indice_parametro + len(parametro_busca) + 1
@@ -43,12 +63,13 @@ class ExtratorURL:
         return len(self.url)
 
     def __str__(self):
-        return self.url + "\n" + "Parâmetros: " + self.get_url_parametros() + "\n" + "URL Base: " + self.get_url_base()
+        return self.url + "\n" + "URL Base: " + self.get_url_base() + "\n" + "Parâmetros: " + "\n" \
+               + [(self.get_nome_parametro()[x] + " = " + self.get_valor_parametro(self.get_nome_parametro()[x])) for x in range(len(self.get_nome_parametro()))].__str__()
 
     def __eq__(self, other):
         return self.url == other.url
 
-url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+url = "bytebank.com/cambio?quantidade=10000&moedaOrigem=real&moedaDestino=dolar"
 extrator_url = ExtratorURL(url)
 # extrator_url2 = ExtratorURL(url)
 
@@ -59,5 +80,10 @@ print(extrator_url)
 # print(id(extrator_url))
 # print(id(extrator_url2))
 
-valor_quantidade = extrator_url.get_valor_parametro("moedaDestino")
-print(valor_quantidade)
+VALOR_DOLAR = 5.50  # 1 dólar = 5.50 reais
+moeda_origem = extrator_url.get_valor_parametro("moedaOrigem")
+moeda_destino = extrator_url.get_valor_parametro("moedaDestino")
+quantidade = extrator_url.get_valor_parametro("quantidade")
+if moeda_origem == "real":
+    conversao_real_para_dolar = int(quantidade) / VALOR_DOLAR
+    print("Conversão: {:0.2f}".format(conversao_real_para_dolar), " doláres")
