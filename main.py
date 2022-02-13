2fbe5b32c309b81eb5ce968ebda67b86a7b18cac
Thiago Souza <thiagossouza07@gmail.com>
2022-02-13 16:05:00 -0300

    Utilizando Métodos de String

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ print(url_base)  # Vai imprimir “bytebank.com/cambio”
 
 url_parametros = url[20:36]
 print(url_parametros)
+
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+url_parametros = url[indice_interrogacao+1:]
+
+parametro_busca = 'moedaDestino'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+print(valor)
