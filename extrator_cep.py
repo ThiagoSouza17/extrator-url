7baf2b5d8d70132627f22fdbcef71adbca70e28d
Thiago Souza <thiagossouza07@gmail.com>
2022-02-13 16:58:05 -0300

    Aplicando Quantificadores e Intervalos

--- extrator_cep.py
+++ extrator_cep.py
@@ -4,7 +4,7 @@ import re # Regular Expression -- RegEx
 
 # 5 dígitos + hífen (opcional) + 3 dígitos
 
-padrao = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+padrao = re.compile("[0-9]{5}[-]?[0-9]{3}")
 busca = padrao.search(endereco)
 
 if busca:
